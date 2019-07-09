1527cf437cc99bed6db69c9313fbbd19873104a4
Gavin M. Roy <gavinr@aweber.com>
2019-07-09 18:21:50 -0400

    CI/build/testing/deploy updates

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
-import setuptools
+import os
+import pathlib
+import subprocess
+import sys
 
-setuptools.setup()
+import setuptools
+from setuptools.command import build_ext
+
+if bool(os.environ.get('USE_CYTHON')):
+    from Cython.Build import cythonize
+else:
+    cythonize = None
+
+LIBPG_QUERY = str(pathlib.Path('./libpg_query'))
+
+
+class BuildExt(build_ext.build_ext):
+    def run(self):
+        return_code = subprocess.call(['make', '-C', LIBPG_QUERY, 'build'])
+        if return_code:
+            sys.stderr.write('libpg_query failed to build')
+            sys.exit(return_code)
+        super().run()
+
+
+ext_modules = [
+    setuptools.Extension(
+        'pgparse',
+        ['pgparse.c' if cythonize is None else 'pgparse.pyx'],
+        libraries=['pg_query'],
+        include_dirs=[LIBPG_QUERY],
+        library_dirs=[LIBPG_QUERY])]
+
+setuptools.setup(
+    cmdclass={'build_ext': BuildExt},
+    ext_modules=ext_modules if cythonize is None else
+    cythonize(
+        ext_modules,
+        compiler_directives={'linetrace': bool(os.environ.get('TEST_BUILD'))}))
