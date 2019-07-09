442a5ebae4dd79129ece891aee5e5c1f15f3ab27
Gavin M. Roy <gavinr@aweber.com>
2019-07-09 18:47:44 -0400

    Attempt to fix coverage

--- ci/test.sh
+++ ci/test.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 set -e
+flake8 --output build/flake8.txt --tee
 sed -i.bak 's/# cython: language_level=3/# cython: language_level=3, linetrace=True/' pgparse.pyx
 rm -f pgparse.pyx.bak
-USE_CYTHON=1 python setup.py build_ext -i --define CYTHON_TRACE_NOGIL
-flake8 --output build/flake8.txt --tee
+export USE_CYTHON=1
+python setup.py build_ext --force --inplace --define CYTHON_TRACE_NOGIL
 coverage run setup.py nosetests
 coverage report -m pgparse.pyx
 coverage xml -o build/coverage.xml pgparse.pyx
